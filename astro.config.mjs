3e5257c1fb34363d2c84505b43c176113654097a
Draazen <barberisvalentin631@gmail.com>
2024-12-02 10:17:42 -0300

    Avanzando en el proyecto.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import preact from "@astrojs/preact";
 
-import tailwind from '@astrojs/tailwind';
-
-import preact from '@astrojs/preact';
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind(), preact(), auth()],
   i18n: {
     defaultLocale: 'es',
-    locales: ['es', 'ca'],
+    locales: ['es', 'en', 'ca'],
     routing: {
       prefixDefaultLocale: false
     }
-  }
+  },
+  output: "server",
+  adapter: vercel()
 });
